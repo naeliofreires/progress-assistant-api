fa47b5ee414841cf2641f016343ac40ffa61840a
Naélio Freires <naeliosistemas@gmail.com>
2022-03-17 19:49:50 -0300

    db config - 1.5

--- config/database.js
+++ config/database.js
@@ -1,13 +1,36 @@
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST", "127.0.0.1"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "dev"),
+//       user: env("DATABASE_USERNAME", "naeliofreires"),
+//       password: env("DATABASE_PASSWORD", "codigolimpo"),
+//       ssl: env.bool("DATABASE_SSL", false),
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "dev"),
-      user: env("DATABASE_USERNAME", "naeliofreires"),
-      password: env("DATABASE_PASSWORD", "codigolimpo"),
-      ssl: env.bool("DATABASE_SSL", false),
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        username: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi123"),
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      options: {
+        ssl: true,
+      },
     },
   },
 });
