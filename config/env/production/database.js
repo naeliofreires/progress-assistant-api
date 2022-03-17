c06239f782487882e646008ed94c4d8063da3df4
Naélio Freires <naeliosistemas@gmail.com>
2022-03-17 19:29:09 -0300

    db config - 1.2

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,37 +1,19 @@
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: "postgres",
-//     connection: {
-//       host: env("DATABASE_HOST", "127.0.0.1"),
-//       port: env.int("DATABASE_PORT", 5432),
-//       database: env("DATABASE_NAME", "dev"),
-//       user: env("DATABASE_USERNAME", "naeliofreires"),
-//       password: env("DATABASE_PASSWORD", "codigolimpo"),
-//       ssl: env.bool("DATABASE_SSL", false),
-//     },
-//   },
-// });
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
 
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "postgres",
-          host,
-          port,
-          database,
-          username: user,
-          password,
-          ssl: { rejectUnauthorized: false },
-        },
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false,
       },
     },
-  };
-};
+    debug: false,
+  },
+});
