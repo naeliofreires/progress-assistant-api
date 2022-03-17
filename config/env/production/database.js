02c4d3d23dbb24ecb924b7df47bbef4c234e6300
Naélio Freires <naeliosistemas@gmail.com>
2022-03-17 18:39:58 -0300

    db config - 0.9

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,42 +1,34 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
-
-console.log("=> => => @nf", {
-  host: config.host,
-  port: config.port,
-  database: config.database,
-  user: config.user,
-  password: config.password,
-});
-
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "dev"),
-      user: env("DATABASE_USERNAME", "naeliofreires"),
-      password: env("DATABASE_PASSWORD", "codigolimpo"),
-      ssl: env.bool("DATABASE_SSL", false),
-    },
-  },
-});
-
 // module.exports = ({ env }) => ({
 //   connection: {
 //     client: "postgres",
 //     connection: {
-//       host: config.host,
-//       port: config.port,
-//       database: config.database,
-//       user: config.user,
-//       password: config.password,
-//       ssl: false,
-//       options: {
-//         ssl: false,
-//       },
+//       host: env("DATABASE_HOST", "127.0.0.1"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "dev"),
+//       user: env("DATABASE_USERNAME", "naeliofreires"),
+//       password: env("DATABASE_PASSWORD", "codigolimpo"),
+//       ssl: env.bool("DATABASE_SSL", false),
 //     },
-//     debug: false,
 //   },
 // });
+
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
+console.log(JSON.stringify(config, null, 4));
+
+module.exports = ({ env }) => {
+  return {
+    connection: {
+      client: "postgres",
+      connection: {
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: env.bool("DATABASE_SSL", false),
+      },
+    },
+  };
+};
