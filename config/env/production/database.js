e3dea7e6e3d2fd9d58e704bbd636cbd36f6084b3
Naélio Freires <naeliosistemas@gmail.com>
2022-03-17 17:11:23 -0300

    db config - 0.2

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -23,9 +23,7 @@ module.exports = ({ env }) => ({
       database: config.database,
       user: config.user,
       password: config.password,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      ssl: env.bool("DATABASE_SSL", false),
     },
     debug: false,
   },
