fc2e242e45e46cbb7cb0c58d8dc4ec32544fcb0f
Naélio Freires <naeliosistemas@gmail.com>
2022-03-17 19:57:34 -0300

    db config - 1.7

--- config/server.js
+++ config/server.js
@@ -2,6 +2,8 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
-    keys: env.array("APP_KEYS"),
+    keys: [
+      "AzTNSR3eSwX4gXQ1NNqLug==,QVpibspSg86ulLNJO3dIIw==,TfyucWvUlC7PR0AqOMRr2w==,E/n6ej4roSlRN4lK6Wh7ug==",
+    ],
   },
 });
